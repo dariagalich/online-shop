8bea519fb0b4acf9776c35326330f23f173f1094
daria <dariagalich22@gmail.com>
2023-09-02 21:37:40 +0300

    перенесла ui, подключила сервер

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductsService} from "./servises/products.service";
+import {Daum} from "./component/product/interface";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent
+  implements OnInit
+{
   title = 'online-shop';
+
+  products: Daum[] = []
+
+  constructor(private _productService: ProductsService) {
+  }
+
+  ngOnInit() {
+    this._productService.getProducts().subscribe(resp => {
+      this.products = resp.data
+      console.log('this.products', this.products)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { CardComponent } from './component/card/card.component';
+import { ProductComponent } from './component/product/product.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    CardComponent,
+    ProductComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
